07a274b6844dd194db4e1e14b93b9dfc90ec17f0
Shubham raj <rshubhamraj09@gmail.com>
2026-04-17 18:58:34 +0530

    Leet_80

---
+++ Leet_80.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//Leetcode problem 80. Remove Duplicates from Sorted Array II
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 2) return nums.size(); // edge case
+        
+        int k = 2;
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) { // not a 3rd duplicate?
+                nums[k] = nums[i];        
+                k++;
+            }
+        }
+        
+        return k;
+    }
+};

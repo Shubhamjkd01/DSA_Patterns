f8d7cc6bb3e40d7936ca010f524df2c71d5b988d
Shubham raj <rshubhamraj09@gmail.com>
2026-04-23 09:24:37 +0530

    M-Aarray

--- Codeforces/1497B_M-Array.cpp
+++ Codeforces/1497B_M-Array.cpp
@@ -26,26 +26,28 @@ int main() {
             groups++;
         }
 
-        for (int r = 1; r <= m/2; r++){
+        for (int r = 1; r <= m/2; r++){    //keep in mind that we are counting pairs of remainders, so we only need to go up to m/2
             if (m-r == r){
-                if (rm[r]>0){
+                if (rm[r]>0){       //ye important hai, kyunki agar rm[r] = 0 hoga toh hume groups count nahi karna hai, warna hum ek extra group count kar denge
                     groups++;
 
                 }
 
             }
             else {
-                int c1 = rm[r];
-                int c2 = rm[m-r];
+                int c1 = rm[r];     //ye important hai, kyunki agar rm[r] = 0 hoga toh hume groups count nahi karna hai, warna hum ek extra group count kar denge
+                int c2 = rm[m-r];  //ye important hai, kyunki agar rm[m-r] = 0 hoga toh hume groups count nahi karna hai, warna hum ek extra group count kar denge
 
-                if (c1 == 0 && c2 == 0) continue;
+                if (c1 == 0 && c2 == 0) continue;    //
 
                 groups ++;
-                int diff = abs(c1-c2);
+                int diff = abs(c1-c2);  //ye important hai, kyunki agar c1 aur c2 equal honge toh hume groups count nahi karna hai, warna hum ek extra group count kar denge
                 if (diff > 0){
-                    groups += diff-1;
+                    groups += diff-1;   //think of it this way, agar c1 aur c2 equal honge toh hume groups count nahi karna hai, warna hum ek extra group count kar denge, isliye hum diff-1 kar rahe hain
 
                 }
+
+                
 
                 
 

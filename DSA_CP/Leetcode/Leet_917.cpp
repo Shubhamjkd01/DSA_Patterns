f8cab03257e51e0ca5f06401e09a68de20f15dab
Shubham raj <rshubhamraj09@gmail.com>
2026-05-07 22:48:51 +0530

    Leet_917

---
+++ DSA_CP/Leetcode/Leet_917.cpp
@@ -0,0 +1,24 @@
+// LeetCode 917. Reverse Only Letters
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while(left < right){
+         if(!isalpha(s[left])){
+             left++;
+            }
+         else if(!isalpha(s[right])){
+            right--;
+            }
+         else{
+             swap(s[left], s[right]);
+              left++;
+            right--;
+            }
+        }
+        return s;
+    }
+};

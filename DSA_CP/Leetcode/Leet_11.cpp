e82547b83db951075759ffd936cef5725cc9f43f
Shubham raj <rshubhamraj09@gmail.com>
2026-05-15 23:27:44 +0530

    Leet_11

---
+++ DSA_CP/Leetcode/Leet_11.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+// LeetCode 11. Container With Most Water
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int left = 0;
+        int right = height.size() - 1;
+
+        int maxi = 0;
+
+        while(left < right)
+        {
+            int width = right - left;
+
+            int h = min(height[left], height[right]);
+
+            int area = width * h;
+
+            maxi = max(maxi, area);
+
+            // Move smaller height
+            if(height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxi;
+    }
+};
